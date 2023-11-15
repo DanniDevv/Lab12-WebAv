15f6b19f90d51c8f8a1e5cbe95635c6b97c4a875
DanniDevv <dante.rodriguez.c@tecsup.edu.pe>
2023-11-15 11:24:30 -0500

    Lab 12 Web Av

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailsComponent } from './posts/post-details/post-details.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'posts', component: PostListComponent },
+  { path: 'posts/:id', component: PostDetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
