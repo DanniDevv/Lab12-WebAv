8e2aa44a12fb5821cc0a3e6ba5cb9af862b38bd5
DanniDevv <dante.rodriguez.c@tecsup.edu.pe>
2023-11-20 20:05:41 -0500

    Web Av modulos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+import { RouterModule } from '@angular/router';
+import { NotFountComponent } from './not-fount/not-fount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NotFountComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PostsModule
+    PostsModule,
+    UsersModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
